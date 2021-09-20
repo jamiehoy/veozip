584dd4f1fa68cca29b8b1014477ec0803afc9bd5
Jamie Hoy <zelgados@icloud.com>
2021-09-19 23:06:14 -0700

    Change veozip.py to no longer reference unzipping and add comments

--- veozip.py
+++ veozip.py
@@ -11,26 +11,24 @@ Zips and unzips using built-in zip utility on a mac
 def zip_file(file):
     #Get user
     USER = os.environ["USER"]
+
+    #For timestamp
     now = datetime.datetime.now()
     timestamp = "%s-%s-%s_%s%s%s"%(now.month,now.day,now.year,now.hour,now.minute,now.second)
+
+    #Filename taken in as a argument
     file = sys.argv[1]
     default_location = "/Users/%s/Desktop/"%USER
+
+    #Command String
     zip = "zip -r %s%s-%s.zip %s -x '*.DS_Store' -x '__MACOSX'"%(default_location,file,timestamp,file)
-    #print(zip)
     os.system(zip)
-    #print("COMMAND: %s"%zip)
     print("%s-%s.zip saved to: %s"%(file,timestamp,default_location))
 
-def unzip_file(file):
-    print("in unzip_file.. exiting..")
-    sys.exit()
-
 def menu(file):
-    choice = int(input("1: zip file/folder\n2: unzip file/folder\n0:Quit\n"))
+    choice = int(input("1: zip file/folder\n0: Quit\n"))
     if choice == 1:
         zip_file(file)
-    elif choice == 2:
-        unzip_file(file)
     elif choice == 0:
         print("exiting...")
         sys.exit()
@@ -45,5 +43,3 @@ if __name__ == "__main__":
     except IndexError:
         print("Requires file/folder name.")
         sys.exit()
-
-
